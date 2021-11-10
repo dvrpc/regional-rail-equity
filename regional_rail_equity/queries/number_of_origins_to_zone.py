e2e5789d41d958fe52a2694105ccd3f5fee72ff6
aaronfraint <aaronfraint@icloud.com>
2021-11-10 15:31:05 -0500

    initial test case with data

---
+++ regional_rail_equity/queries/number_of_origins_to_zone.py
@@ -0,0 +1,37 @@
+from regional_rail_equity import db
+from regional_rail_equity.zones.zones import CENTER_CITY
+
+if __name__ == "__main__":
+    query = f"""
+    with zones as (
+        select
+            tazt,
+            geom
+        from
+            taz_2010 
+        order by
+            tazt
+    ),
+    origins as (
+        select
+            fromzone,
+            sum(mat2000) as mat2000sum,
+            sum(mat2200) as mat2200sum
+        from
+            existing_2019_am_hwy_transit_od 
+        where tozone::int in {CENTER_CITY}
+        group by fromzone
+    )
+
+    select
+        z.tazt, z.geom,
+        o.fromzone, o.mat2000sum, o.mat2200sum
+    from
+        zones z
+    inner join
+        origins o
+    on
+        z.tazt = o.fromzone
+    """
+
+    db.gis_make_geotable_from_query(query, "trips_into_center_city_am", "POLYGON", 26918)
