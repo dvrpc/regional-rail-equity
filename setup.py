5e04f46f92d35aa9c2b94e68862621590d549a56
aaronfraint <aaronfraint@icloud.com>
2021-11-10 11:23:11 -0500

    add boilerplate

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="regional_rail_equity",
+    packages=find_packages(),
+    version="0.0.1",
+    description="Python package analyze regional model outputs with an equity lens",
+    author="Aaron Fraint, AICP",
+    license="GPLv3",
+)
