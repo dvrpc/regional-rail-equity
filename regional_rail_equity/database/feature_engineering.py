8563bfa93b9d7c110a574f9980aa36c105e1905a
aaronfraint <aaronfraint@icloud.com>
2021-11-24 16:04:23 -0500

    summarize CTPP data and generate PA county layer

--- regional_rail_equity/database/feature_engineering.py
+++ regional_rail_equity/database/feature_engineering.py
@@ -3,7 +3,7 @@ from pg_data_etl import Database
 from regional_rail_equity import db
 from regional_rail_equity.helpers import print_title
 
-ALL_SCHEMAS = ["computed"]
+ALL_SCHEMAS = ["computed", "ctpp"]
 
 
 @print_title("CREATING NEW SCHEMAS")
@@ -19,5 +19,71 @@ def create_schemas(db: Database) -> None:
         db.schema_add(schema)
 
 
+@print_title("GENERATING A SUMMARY OF THE CTPP EQUITY TABLES")
+def summarize_ctpp_data(db: Database):
+    """
+    Execute a query that combines all of the raw CTPP
+    tables into a single table with necessary values
+
+    Summary includes:
+        - percent of population that does not speak english at home
+
+    To Do:
+        - income
+        - race
+        - vehicles available
+    """
+
+    query_for_new_table = """
+        with ctpp_taz_shapes as (
+            select
+                name,
+                split_part(split_part(name, ',', 1), ' ', 2)::int as taz_id,
+                geom
+            from
+                public.ctpp_language_at_home 
+        ),
+        english as (
+            select name,
+                case when est_total > 0 then
+                    (1 - (est_english / est_total)) * 100
+                else null end as pct_non_english
+            from public.ctpp_language_at_home 
+        )
+        select
+            s.name, s.taz_id, s.geom,
+            e.pct_non_english,
+            width_bucket(e.pct_non_english, 0, 100, 5) as bucket_pct_non_english
+        from ctpp_taz_shapes s
+        full outer join english e on s.name = e.name
+    """
+
+    query = f"""
+        update
+            ctpp.summary
+        set
+            bucket_pct_non_english = 5
+        where
+            bucket_pct_non_english > 5;
+    """
+
+    db.gis_make_geotable_from_query(query_for_new_table, "ctpp.summary", "POLYGON", 26918)
+    db.execute(query)
+
+
+@print_title("EXTRACTING DVRPC PA COUNTIES FROM MULTI-STATE COUNTY LAYER")
+def clip_counties(db: Database):
+    query = """
+        select * from dvrpc_counties
+        where
+            state_name = 'Pennsylvania'
+        and
+            dvrpc_reg = 'Yes'
+    """
+    db.gis_make_geotable_from_query(query, "dvrpc_pa_counties", "POLYGON", 26918)
+
+
 if __name__ == "__main__":
     create_schemas(db)
+    summarize_ctpp_data(db)
+    clip_counties(db)
