c0cb60e107df8d6a2035086aafbf7aa2ac0ec91c
aaronfraint <aaronfraint@icloud.com>
2021-11-24 10:58:27 -0500

    add initial feature engineering script

---
+++ regional_rail_equity/database/feature_engineering.py
@@ -0,0 +1,23 @@
+from pg_data_etl import Database
+
+from regional_rail_equity import db
+from regional_rail_equity.helpers.printout import print_title
+
+ALL_SCHEMAS = ["computed"]
+
+
+@print_title("CREATING NEW SCHEMAS")
+def create_schemas(db: Database) -> None:
+    """
+    Create schemas that will be needed later
+
+    Arguments:
+        db (Database): analysis database
+    """
+
+    for schema in ALL_SCHEMAS:
+        db.schema_add(schema)
+
+
+if __name__ == "__main__":
+    create_schemas(db)
