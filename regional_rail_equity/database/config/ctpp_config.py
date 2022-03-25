ddaf68d41dc0cb113e639acb1550c7320537fb83
mmorley <mmorley0395@gmail.com>
2022-03-25 15:00:00 -0400

    updated config file with new tables/columns

--- regional_rail_equity/database/config/ctpp_config.py
+++ regional_rail_equity/database/config/ctpp_config.py
@@ -18,77 +18,83 @@ poverty = {
     ],
 }
 
-# TODO: Minority status
-
-
-# TODO: remove this one below
-# race = {
-#     "filepath": "EA_A101108 - Race (5) (All persons)/C13.shp",
-#     "sql_tablename": "data.ctpp_race_all_people",
-#     "names": [
-#         "ctpp_id",
-#         "label",
-#         "name",
-#         "geoid",
-#         "est_all_races",
-#         "moe_all_races",
-#         "est_white_alone",
-#         "moe_white_alone",
-#         "est_black_alone",
-#         "moe_black_alone",
-#         "est_asian_alone",
-#         "moe_asian_alone",
-#         "est_other",
-#         "moe_other",
-#         "geometry",
-#     ],
-# }
-
-# TODO: modify the columns for this one and add both NJ and PA tables
-english = {
-    "filepath": "EA_A117200 - Ability to speak English (3) by Language spoken at home (13) (Persons 5 years old and over in households)/C13.shp",
-    "sql_tablename": "data.ctpp_language_at_home",
+minority = {
+    "filepath": "EA_A101105 - Minority Status (3) (All Persons)/C13.shp",
+    "sql_tablename": "data.ctpp_minority_by_person",
     "names": [
         "ctpp_id",
         "label",
         "name",
         "geoid",
-        "est_total",
-        "moe_total",
-        "est_english",
-        "moe_english",
-        "est_spanish",
-        "moe_spanish",
-        "est_french_hatian_cajun",
-        "moe_french_hatian_cajun",
-        "est_germanic",
-        "moe_germanic",
-        "est_russian_slavic",
-        "moe_russian_slavic",
-        "est_indo_european",
-        "moe_indo_european",
-        "est_korean",
-        "moe_korean",
-        "est_chinese",
-        "moe_chinese",
-        "est_vietnamese",
-        "moe_vietnamese",
-        "est_tagalog",
-        "moe_tagalog",
-        "est_other_aapi",
-        "moe_other_aapi",
-        "est_arabic",
-        "moe_arabic",
+        "est_total_persons",
+        "moe_total_persons",
+        "est_white_alone_non_hispanic",
+        "moe_white_alone_non_hispanic",
         "est_other",
         "moe_other",
         "geometry",
     ],
 }
+english_column_names = [
+    "ctpp_id",
+    "label",
+    "name",
+    "geoid",
+    "est_total_persons",
+    "est_english_speakers_five_over",
+    "geometry",
+]
 
+english_nj = {
+    "filepath": "NJ - EA_A117200 - Ability to speak English (3) by Language spoken at home (13) (Persons 5 years old and over in households)/C13.shp",
+    "sql_tablename": "data.nj_language_at_home",
+    "names": english_column_names,
+}
 
+english_pa = {
+    "filepath": "PA - EA_A117200 - Ability to speak English (3) by Language spoken at home (13) (Persons 5 years old and over in households)/C13.shp",
+    "sql_tablename": "data.pa_language_at_home",
+    "names": english_column_names,
+}
 # TODO: add vehicles available table for both NJ and PA
+
+vehicles_available_column_names = [
+    "ctpp_id",
+    "label",
+    "name",
+    "geoid",
+    "est_total_vehicles_available",
+    "moe_total_vehicles_available",
+    "est_zero_vehicles_available",
+    "moe_zero_vehicles_available",
+    "est_one_vehicles_available",
+    "moe_one_vehicles_available",
+    "est_two_vehicles_available",
+    "moe_two_vehicles_available",
+    "est_three_vehicles_available",
+    "moe_three_vehicles_available",
+    "est_four_plus_vehicles_available",
+    "moe_four_plus_vehicles_available",
+    "geometry",
+]
+
+vehicles_available_nj = {
+    "filepath": "NJ - EA_A104201 - Vehicles available (6) by Poverty status (4) (Workers 16 years and over in households for whom poverty status is determined)/C13.shp",
+    "sql_tablename": "data.nj_vehicles_available",
+    "names": vehicles_available_column_names,
+}
+
+vehicles_available_pa = {
+    "filepath": "PA - EA_A104201 - Vehicles available (6) by Poverty status (4) (Workers 16 years and over in households for whom poverty status is determined)/C13.shp",
+    "sql_tablename": "data.pa_vehicles_available",
+    "names": vehicles_available_column_names,
+}
 
 ctpp_configurations = [
     poverty,
-    english,
+    minority,
+    english_nj,
+    english_pa,
+    vehicles_available_nj,
+    vehicles_available_pa,
 ]
