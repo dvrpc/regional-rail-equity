fa5e5422934507345d7c5654171949c23313f8ee
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2022-03-28 11:47:44 -0400

    update ctpp test

---
+++ tests/test_ctpp.py
@@ -0,0 +1,32 @@
+import pytest
+
+from regional_rail_equity import db
+from regional_rail_equity.database.config.ctpp_config import ctpp_configurations
+
+
+@pytest.mark.parametrize(
+    "tablename",
+    [config["sql_tablename"] for config in ctpp_configurations],
+)
+def test_number_of_tazs(tablename):
+    """
+    There are 2,182 traffic analysis zones
+    within the five PA counties inside the
+    DVRPC region. This test confirms
+    that each table has all of the TAZs.
+    """
+    query = f"""
+        select count(*) from {tablename}
+    """
+
+    taz_counts = {"pa": 2182, "nj": 958, "all": 3140}
+
+    result = db.query_as_singleton(query)
+    if "nj" in tablename:
+        expected_tazs = taz_counts["nj"]
+    elif "pa" in tablename:
+        expected_tazs = taz_counts["pa"]
+    else:
+        expected_tazs = taz_counts["all"]
+
+    assert result == expected_tazs, f"{tablename} has the wrong number of tazs: {result}"
