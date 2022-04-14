af7030098b1ee4c4cf9f2da05c0c212bd3f4ef57
aaronfraint <aaronfraint@icloud.com>
2022-04-14 13:53:06 -0400

    import station summary files

---
+++ regional_rail_equity/database/config/station_summary_config.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class StationSummaryFile:
+    filename: str
+    skiprows: int
+    sql_tablename: str
+
+
+nobuild_2045_am = StationSummaryFile(
+    filename="2045_NoBld_AM_from_home_only_Trip_Stats_by_Station.xlsx",
+    skiprows=4,
+    sql_tablename="station_summary_2045nobuild_am",
+)
+
+
+station_summary_files = [
+    nobuild_2045_am,
+]
---
+++ regional_rail_equity/database/load/load_station_summary_tables.py
@@ -0,0 +1,42 @@
+from regional_rail_equity import db
+from regional_rail_equity.database.config.env_vars import GDRIVE_PROJECT_FOLDER
+from regional_rail_equity.database.config.station_summary_config import station_summary_files
+from regional_rail_equity.helpers import print_title, print_msg
+
+TOD_FOLDER = GDRIVE_PROJECT_FOLDER / "Data/Inputs/TOD Station Summaries"
+
+
+@print_title("Importing station GIS data from 'Building on our Strengths' project")
+def import_gis():
+    """
+    Import the shapefile containing TOD analysis results 
+    for the Philly area rail stations from the
+    'Building on our Strengths' report
+    """
+    shp_filepath = TOD_FOLDER / "TOD_Opportunities/TOD_Opportunities.shp"
+    db.import_gis(
+        filepath=shp_filepath,
+        sql_tablename="building_on_our_strengths_stations",
+        gpd_kwargs={"if_exists": "replace"},
+    )
+
+
+@print_title("Importing station-level summary files")
+def import_excel():
+    """
+    Import all of the configured Excel files with station-level summaries
+    """
+    for stationfile in station_summary_files:
+        print_msg(f"Importing {stationfile.filename}")
+
+        db.import_file_with_pandas(
+            filepath=TOD_FOLDER / stationfile.filename,
+            tablename=stationfile.sql_tablename,
+            pd_read_kwargs={"skiprows": stationfile.skiprows},
+            df_import_kwargs={"if_exists": "replace"},
+        )
+
+
+if __name__ == "__main__":
+    import_gis()
+    import_excel()
