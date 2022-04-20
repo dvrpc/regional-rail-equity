a07ffd51a928d27d20a80994716ba606786593d0
mmorley <mmorley0395@gmail.com>
2022-04-20 15:15:03 -0400

    added station summary

--- regional_rail_equity/analysis/summarize_stations_tod.py
+++ regional_rail_equity/analysis/summarize_stations_tod.py
@@ -1,16 +1,32 @@
 from regional_rail_equity import db
-from regional_rail_equity.database.config.path_legs_config import path_legs_config
+from regional_rail_equity.database.config.station_summary_config import (
+    station_summary_files,
+)
 
-query = """
+query_template = """
+    drop table if exists NEW_TABLENAME;
+
+    create table NEW_TABLENAME as
+    
     with joined_model_with_tod as(
-        select *, st_transform(a.geom, 4326) from model_rr_station_stoppoints a
+        select *, st_transform(a.geom, 4326) 
+        from model_rr_station_stoppoints a
         right join building_on_our_strengths_stations b
         on st_dwithin(a.geom,b.geom,110)
-        inner join station_summary_2045nobuild_am c 
+        inner join TABLENAME_PLACEHOLDER c 
         on a.no = c.stoppointno
         where b.type = 'Commuter Rail' and "operator" = 'SEPTA')
-    select * from joined_model_with_tod
+    select station, "no", lucontext, type_1, passboardap as "boardings", passalightap as "alights", "st_transform"  from joined_model_with_tod
+    order by station
      """
 
+
 if __name__ == "__main__":
-    pass
+
+    for scenario in station_summary_files:
+        existing_raw_table = scenario.sql_tablename
+        new_tablename = existing_raw_table.replace("public.", "computed.")
+        query = query_template.replace(
+            "TABLENAME_PLACEHOLDER", scenario.sql_tablename
+        ).replace("NEW_TABLENAME", new_tablename)
+        db.execute(query)
--- regional_rail_equity/database/config/station_summary_config.py
+++ regional_rail_equity/database/config/station_summary_config.py
@@ -11,7 +11,7 @@ class StationSummaryFile:
 nobuild_2045_am = StationSummaryFile(
     filename="2045_NoBld_AM_from_home_only_Trip_Stats_by_Station.xlsx",
     skiprows=4,
-    sql_tablename="station_summary_2045nobuild_am",
+    sql_tablename="public.station_summary_2045nobuild_am",
 )
 
 
