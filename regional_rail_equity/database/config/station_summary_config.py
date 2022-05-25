7d88568c562fdcefb904ef523ed91801c1c7ebf5
mmorley <mmorley0395@gmail.com>
2022-05-25 09:04:19 -0400

    updated summary with new lookup col "old_num"

--- regional_rail_equity/database/config/station_summary_config.py
+++ regional_rail_equity/database/config/station_summary_config.py
@@ -18,74 +18,74 @@ nobuild_2045_am = StationSummaryFile(
 
 # ----------Scenario 1 ----------------------------------------------------------
 s1_am = StationSummaryFile(
-    filename="2045_Scenario1_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario1_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="AM",
     sql_tablename="public.station_summary_2045s1_am",
 )
 s1_md = StationSummaryFile(
-    filename="2045_Scenario1_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario1_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="MD",
     sql_tablename="public.station_summary_2045s1_md",
 )
 s1_pm = StationSummaryFile(
-    filename="2045_Scenario1_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario1_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="PM",
     sql_tablename="public.station_summary_2045s1_pm",
 )
 s1_nt = StationSummaryFile(
-    filename="2045_Scenario1_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario1_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="NT",
     sql_tablename="public.station_summary_2045s1_nt",
 )
 # ----------Scenario 2 ----------------------------------------------------------
 s2_am = StationSummaryFile(
-    filename="2045_Scenario2_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario2_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="AM",
     sql_tablename="public.station_summary_2045s2_am",
 )
 s2_md = StationSummaryFile(
-    filename="2045_Scenario2_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario2_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="MD",
     sql_tablename="public.station_summary_2045s2_md",
 )
 s2_pm = StationSummaryFile(
-    filename="2045_Scenario2_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario2_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="PM",
     sql_tablename="public.station_summary_2045s2_pm",
 )
 s2_nt = StationSummaryFile(
-    filename="2045_Scenario2_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario2_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="NT",
     sql_tablename="public.station_summary_2045s2_nt",
 )  # ----------Scenario 3 ----------------------------------------------------------
 s3_am = StationSummaryFile(
-    filename="2045_Scenario3_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario3_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="AM",
     sql_tablename="public.station_summary_2045s3_am",
 )
 s3_md = StationSummaryFile(
-    filename="2045_Scenario3_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario3_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="MD",
     sql_tablename="public.station_summary_2045s3_md",
 )
 s3_pm = StationSummaryFile(
-    filename="2045_Scenario3_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario3_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="PM",
     sql_tablename="public.station_summary_2045s3_pm",
 )
 s3_nt = StationSummaryFile(
-    filename="2045_Scenario3_NewFare_RegRail_Trip_Stats.xlsx",
+    filename="RE_2045_Scenario3_NewFare_RegRail_Trip_Stats.xlsx",
     skiprows=4,
     sheetname="NT",
     sql_tablename="public.station_summary_2045s3_nt",
